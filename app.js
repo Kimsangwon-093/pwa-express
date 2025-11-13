cebad6999f1c5ed769b78e72818eb4b63b21aa6c
Kimsangwon-093 <akg9380@daum.net>
2025-11-13 18:54:13 +0900

    251113 route ~ sequelize query

--- app.js
+++ app.js
@@ -1,112 +1,46 @@
-// express 기본 구조 
+// // express 기본 구조 
 
-import express,{ request, response } from 'express';  //express 모듈 가져오기
-import authRouter from './routes/auth.router.js';
 import usersRouter from './routes/users.router.js';
 import { eduTest, eduUsersTest } from './app/middlewares/edu/edu.middleware.js';
 import { errorHandler } from './app/middlewares/errors/error-handler.js';
+import eduRouter from './routes/edu.router.js';
 
 const app = express();
-app.use(express.json()); //JSON으로 요청이 올 경우 파싱 처리
-app.use(eduTest);
+app.use(express.json()); // JSON으로 요청이 올 경우 파싱 처리
+app.use(eduTest); // 커스텀 미들웨어 전역 등록
+// app.use(eduTest); // 커스텀 미들웨어 전역 등록
 
-
-// 파싱(parsing) : 특정 형태를 원하는 형태로 만드는 것
-
-// 클라이언트가 '/api/hi' 경로로 GET 요청을 보낼 때 실행되는 Router 
+// 클라이언트가 '/api/hi' 경로로 GET 요청을 보낼 때 실행되는 Router
 app.get('/api/hi', (request, response, next) => {
-    response.status(200).send('안녕 익스프레스!');
-});
 
-// 클라이언트가 '/api/hi' 경로로 POST 요청을 보낼 때 실행되는 Router
-app.post('/api/hi',(request, response, next) => {
-  response.status(200).send('포스트 익스프레스!');
-});
-
-// 클라이언트가 '/api/hi' 경로로 PUT 요청을 보낼 때 실행되는 Router
-app.put('/api/hi',(request, response, next) => {
-  response.status(200).send('풋 익스프레스!');
-});
-
-// 클라이언트가 '/api/hi' 경로로 PATCH 요청을 보낼 때 실행되는 Router
-app.patch('/api/hi',(request, response, next) => {
-  response.status(200).send('패치 익스프레스!');
-});
-
-// // 클라이언트가 '/api/hi' 경로로 DELETE 요청을 보낼 때 실행되는 Router
-// app.delete('/api/hi',(request, response, next) => {
-//   response.status(200).send('딜리트 익스프레스!');
-// });
-
-// 클라이언트가 '/api/hi' 경로로 DELETE 요청을 보낼 때 실행되는 Router
-app.delete('/api/hi',(request, response, next) => {
-  response.status(200).send({
-    code:'00', 
-    msg:'딜리트 익스프레스!'
-  });
-});
-
-// -----------
-// Query Parameter 제어
-// Request.query 프로퍼티를 통해서 접근 가능
-app.get('/api/posts',(request, response, next) => {
-  const params = request.query;
-  const name = request.query.name;
-  const age = request.query.age;
-  console.log(name, age);
-
-  response.status(200).send(params);
-});
-
-// Segment Parameter
-// `Request.params` 를 통해서 접근 가능
-app.get('/api/posts/:id', (request, response, next) => {
-  const postId = request.params.id;
-  console.log(typeof(postId));
-  response.status(200).send(postId);
-});
-
-// JSON 요청제어
-app.post('/api/posts', (request, response, next) => {
-  const {account, password, name} = request.body;
-  // const account = request.body.account;
-  // const password = request.body.password;
-  // const name = request.body.name;
-//   response.status(200).send({account, password, name});
-
-//   response.status(200).send({
-//     password:password
-//     ,account:account
-//     ,name:name
-//   });
-});
-
-// --------------
-// 라우트 그룹
-// --------------
 // 라우트를 모듈로 나누고 그룹핑하여 관리
-app.use('/api',authRouter);
-app.use('/api/users',eduUsersTest,usersRouter);
-
+app.use('/api', authRouter);
+app.use('/api/users', eduUsersTest, usersRouter);
+app.use(eduRouter);
+});
 // 에러 테스트용 라우트
-app.get('/',(request,response,next) => {
-  // throw를 이용하여 에러 핸들링 처리도 가능 (비동기 처리 내부에서는 사용하면 에러 핸들러가 핸들링 못함)
+app.get('/error', (request, response, next) => {
+  // `throw`를 이용하여 에러 핸들링 처리도 가능 (비동기 처리 내부에서는 사용하면 에러 핸들러가 핸들링 못함)
+  // throw new Error('쓰로우로 예외 발생');
+
+  // 비동기 처리 내부에서는 반드시 `next(error)`를 이용해야 서버 crashed 안 일어남
   setTimeout(() => {
-    next(new Error('쓰로우로 예외발생'));
+    next(new Error('쓰로우로 예외 발생'));
   }, 1000);
 });
 
-// // 대체 라우트(모든 라우터 중에 가장 마지막에 작성)
+// -------------
+// 대체 라우트(모든 라우터 중에 가장 마지막에 작성)
 app.use((request, response, next) => {
   response.status(404).send({
-    code:'E01',
-    msg:'찾을 수 없는 페이지 입니다'
+    code: 'E01',
+    msg: '찾을 수 없는 페이지입니다.',
   });
 });
 
-// ------------------
-// error Handler 등록
-// ------------------
+//------------------
+// Error Handler 등록
+// -----------------
 app.use(errorHandler);
 
 // 서버를 주어진 포트에서 시작
